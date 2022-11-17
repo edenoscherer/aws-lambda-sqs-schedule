9b89aee3919f2468ba3af2fdb4a4d019a7dd134b
Edeno Luiz Scherer <edenoscherer@gmail.com>
2022-11-17 16:13:25 -0300

    change aws region

--- queue-app/src/app.ts
+++ queue-app/src/app.ts
@@ -3,5 +3,3 @@ import { SQSEvent } from 'aws-lambda';
 export const handler = async (event: SQSEvent) => {
     console.log(JSON.stringify(event, null, 2));
 };
-
-// aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/408867156064/sam-app-MySqsQueue-qOJFYTvdOYlI --message-body "Test message"
